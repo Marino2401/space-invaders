a5e560eb45cebe93f407ed67dc384b4b34228e32
Marino2401 <159966620+Marino2401@users.noreply.github.com>
2024-05-03 19:18:44 +0000

    version 8

--- include/Dibujo.hpp
+++ include/Dibujo.hpp
@@ -1,32 +1,78 @@
 #include <fstream>
 #include <ftxui/dom/elements.hpp>
-
+#pragma once
+#include <string>
+#include <list>
+#include <ftxui/screen/screen.hpp>
 using namespace std;
 using namespace ftxui;
 
 class Dibujo
 {
-    private:
+private:
     fstream archivo;
     Element contenido;
-    public:
+    int posicionX;
+    int posicionY;
+    list<string> contenido;
+public:
     Dibujo(string path)
     {
         archivo.open(path);
-
         Elements lineas;
-        while(!archivo.efo())
+        while (!archivo.eof())
+    Dibujo() {
+        this->posicionX = 0;
+        this->posicionY = 0;
+    }
+    Dibujo(int posicionX, int posicionY) {
+        this->posicionX = posicionX;
+        this->posicionY = posicionY;
+    }
+    Dibujo(list<string> contenido,int posicionX, int posicionY) {
+        this->contenido = contenido;
+        this->posicionX = posicionX;
+        this->posicionY = posicionY;
+    }
+
+    void Dibujar(ftxui::Screen &Pantalla){
+
+        int posicionPalabraY = 0;
+        for (auto &&linea : contenido)
         {
             string linea;
-            getline(archivo,linea);
+            getline(archivo, linea);
             lineas.emplace_back(text(linea));
+            int posicionPalabraX = 0;
+            for (auto &&letra : linea)
+            {
+                int posicionFinalX =
+                    this->posicionX + posicionPalabraX;
+                int posicionFinalY =
+                    this->posicionY + posicionPalabraY;
+
+                Pantalla.PixelAt(
+                            posicionFinalX,
+                            posicionFinalY)
+                    .character = letra;
+                posicionPalabraX++;
+            }
+            posicionPalabraY++;
         }
         this->contenido = vbox(lineas);
         archivo.close();
     }
-    Element GetElement()
-    {
+    Element GetElement(){
         return this->contenido;
+    void DesplazarX(int distancia){
+        posicionX += distancia;
     }
-    ~Dibujo(){}
+    void DesplazarY(int distancia){
+        posicionY += distancia;
+    }
+    void EstablecerPosicion(int x, int y){
+        this->posicionX = x;
+        this->posicionY = y;
+    }
+    ~Dibujo() {}
 };
--- src/Ventana.cpp
+++ src/Ventana.cpp
@@ -1,31 +1,73 @@
 #include <iostream>
 #include <string>
 #include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen_interactive.hpp>
-#include<thread>
-using namespace std;
+#include <ftxui/screen/screen.hpp>
+#include <Dibujo.hpp>
+#include <Archivo.hpp>
+#include <thread>
+#include <list>
+#include <experimental/random>
 using namespace ftxui;
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int posicionInicialX = 0;
-    int posicionInicialY = 0;
-     string prueba = "DVD";
+    Archivo alien("./assets/images/alien.txt");
 
-    while(true)
+    list<Dibujo> dibujos;
+    // Crear Aliens
+    for (size_t i = 0; i < 20; i++)
     {
-        posicionInicialY++;
-        this_thread::sleep_for(0.1s);
-        auto pantalla = Screen::Create(Dimension::Fixed(10),Dimension::Fixed(10));
-        Render(Pantalla,border(text("Hola")));
-        Pantalla.PixelAt(5,5).character = 'm';
-        
-        for (auto &&letra : prueba)
-        {
-            Pantalla.PixelAt(posicionX,posicionY).character = letra;
-            posicionX++;
-        }   
-        Pantalla.Print();
+        Dibujo d = alien.CrearDibujo();
+        d.EstablecerPosicion(
+            experimental::randint(0,20),
+            experimental::randint(0,20)
+        );
+        dibujos.push_back(d);
     }
+
+    // Crear monitos
+    for (int i = 0; i < 20; i++)
+    {
+        list<string> c;
+        c.push_back("Personaje:" + to_string(i));
+        c.push_back("  O");
+        c.push_back("--|-- ");
+        c.push_back(" / \\");
+
+        Dibujo contenido(
+            c,
+            experimental::randint(0,50), // PosX
+            experimental::randint(0,50)  // PosY
+        );
+        dibujos.push_back(contenido);
+    }    
+
+    auto Pantalla = Screen::Create(
+        Dimension::Full(), 
+        Dimension::Full()
+    );
+
+    while(true){
+        Pantalla.Clear();
+        this_thread::sleep_for(0.1s);
+
+        // Actualizar
+        for (auto &&i : dibujos)
+        {
+            i.DesplazarX(experimental::randint(-1,1));
+            i.DesplazarY(experimental::randint(-1,1));
+        }
+
+        // Dibujar
+        for (auto &&i : dibujos)
+        {
+            i.Dibujar(Pantalla);
+        }
+
+        Pantalla.Print();
+        cout<<Pantalla.ResetPosition();          
+    }
+
     return 0;
 }
